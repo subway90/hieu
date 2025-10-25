827fc06e38e80863200fa017252a273e53b362e4
subway90 <nguyenhieu3105@gmail.com>
2025-10-25 20:53:17 +0700

    update

--- public/view/home.php
+++ public/view/home.php
@@ -1,33 +1,38 @@
 <link rel="stylesheet" href="<?= URL_P_V ?>css/home.css">
 
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-12 col-lg-6 animate__animated animate__bounceInLeft">
+<div class="container px-0 mt-5">
+    <div class="box-introduce d-flex align-items-center">
+        <div class="col-12 animate__animated animate__bounceInLeft">
             <h2 class="box-introduce-title">
                 <?= WEB_NAME ?>
             </h2>
-            <div id="introduce-desc" class="box-introduce-desc mt-3">
+            <div id="introduce-desc" class="box-introduce-desc mt-3 w-50">
                 <?= WEB_DESCRIPTION ?>
             </div>
+        </div>
         </div>
     </div>
 </div>
 
 <div class="container mt-5">
     <div class="d-flex flex-column flex-lg-row gap-4">
-        <a href="/du-an" class="w-100 bg-box bg-box-btn rounded-5 d-flex align-items-center gap-2 justify-content-center py-3 small animate__animated animate__bounceIn animate__delay-2s">
+        <a href="/du-an"
+            class="w-100 bg-box bg-box-btn rounded-5 d-flex align-items-center gap-2 justify-content-center py-3 small animate__animated animate__bounceIn animate__delay-2s">
             <i class="bi bi-collection"></i>
             <div class="">Tất cả dự án</div>
         </a>
-        <a href="/suu-tam" class="w-100 bg-box bg-box-btn rounded-5 d-flex align-items-center gap-2 justify-content-center py-3 small animate__animated animate__bounceIn animate__delay-3s">
+        <a href="/suu-tam"
+            class="w-100 bg-box bg-box-btn rounded-5 d-flex align-items-center gap-2 justify-content-center py-3 small animate__animated animate__bounceIn animate__delay-3s">
             <i class="bi bi-globe2"></i>
             <div class="">Thiết kế website</div>
         </a>
-        <a href="/suu-tam" class="w-100 bg-box bg-box-btn rounded-5 d-flex align-items-center gap-2 justify-content-center py-3 small animate__animated animate__bounceIn animate__delay-4s">
+        <a href="/suu-tam"
+            class="w-100 bg-box bg-box-btn rounded-5 d-flex align-items-center gap-2 justify-content-center py-3 small animate__animated animate__bounceIn animate__delay-4s">
             <i class="bi bi-layout-wtf"></i>
             <div class="">Thiết kế Figma</div>
         </a>
-        <a href="/dang-nhap" class="w-100 bg-box bg-box-btn rounded-5 d-flex align-items-center gap-2 justify-content-center py-3 small animate__animated animate__bounceIn animate__delay-5s">
+        <a href="/dang-nhap"
+            class="w-100 bg-box bg-box-btn rounded-5 d-flex align-items-center gap-2 justify-content-center py-3 small animate__animated animate__bounceIn animate__delay-5s">
             <i class="bi bi-envelope"></i>
             <div class="">Gửi thư liên hệ</div>
         </a>
